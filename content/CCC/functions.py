0467f08cacc2d77ae69697a9a4dba35cbf24d7b6
Sarah Townson <26084137+Isioviel@users.noreply.github.com>
2024-04-29 16:47:47 +0100

    minor edits

--- content/CCC/functions.py
+++ content/CCC/functions.py
@@ -3,12 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # create some useful functions to use during this workshop
-def convert(image):
+def convert(image, water=True):
     # open the original image, and show its filetype, colour mode and size
     original = Image.open(image)
     print("original:", original.format, original.mode, original.size)
     # the filetype should be JPEG or PNG; the colour mode should be RGB
 
+    
     pixelMap = original.load()      #loading the pixel map of the image
     horizontal, vertical = original.size[0], original.size[1]   # name horizontal and vertical, to make next code clearer
 
@@ -20,12 +21,15 @@ def convert(image):
             # check if r, g & b are similar in value (therefore grey)
             if (r in range (g-10, g+10) and b in range (g-10, g+10)):
                 # if grey, reset to black (to make sure it show up properly in the next section)
-                pixelMap[i,j] = (0, 0, 0)
+                if water == True:
+                    pixelMap[i,j] = (0, 0, 0)
+                elif water == False:
+                    pixelMap[i,j] = (255, 255, 255)
     
     # convert into a numpy array containing all the pixels except the top 25 rows to cut off the header, and show its data type and shape
     np_img = np.asarray(original)
     np_img = np.delete(np_img, slice(0, 25), axis=0)
-    np_img = np.delete(np_img, slice(-25, -1), axis=0)
+    np_img = np.delete(np_img, slice(-26, -1), axis=0)
     print("array: ", np_img.dtype, np_img.shape)
     # the data type should be uint8, the size should be 50 pixels smaller than the original in one dimension
         
